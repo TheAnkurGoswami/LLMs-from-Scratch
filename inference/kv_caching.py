26db4a2044088d940f135de631d8001fb94755ad
अंkur गोswami <kinglusifer1401@gmail.com>
2025-11-23 00:02:47 +0530

    kv cache logic

---
+++ inference/kv_caching.py
@@ -0,0 +1,24 @@
+import torch
+
+class KeyValueCaching(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.k_proj_cache = None
+        self.v_proj_cache = None
+
+    def prefill(self, k_proj, v_proj):
+        self.k_proj_cache = k_proj
+        self.v_proj_cache = v_proj
+    
+    def update(self, k_proj, v_proj):
+        if self.k_proj_cache is None or self.v_proj_cache is None:
+            self.prefill(k_proj, v_proj)
+        else:
+            self.k_proj_cache = torch.cat(
+                [self.k_proj_cache, k_proj], dim=1
+            )
+            self.v_proj_cache = torch.cat(
+                [self.v_proj_cache, v_proj], dim=1
+            )
+
+        return self.k_proj_cache, self.v_proj_cache
