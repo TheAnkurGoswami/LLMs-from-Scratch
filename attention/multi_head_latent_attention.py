dd2956fa99d665f2f83be9349e5b57496223926b
अंkur गोswami <kinglusifer1401@gmail.com>
2025-12-28 18:30:44 +0530

    MLA with decoupled ROPE and additional projections

--- attention/multi_head_latent_attention.py
+++ attention/multi_head_latent_attention.py
@@ -1,11 +1,16 @@
-from attention.projection import Projection
 import torch
 from torch import Tensor
 
-class MultiHeadLatentAttention(torch.nn.module):
+from attention.projection import Projection
+from attention.scaled_dot_product_attention import ScaledDotProductAttention
+from positional_encoding.rotary import RotaryPositionalEncoding
+
+
+class MultiHeadLatentAttention(torch.nn.Module):
     def __init__(
         self,
         d_model: int,
+        d_q_compression: int,
         d_kv_compression: int,
         num_heads: int,
         dim_q: int | None = None,
@@ -16,6 +21,7 @@ class MultiHeadLatentAttention(torch.nn.module):
     ):
         super().__init__()
         self.d_model: int = d_model  # Store as int, not tensor
+        self.d_q_compression = d_q_compression
         self.d_kv_compression = d_kv_compression
         self.num_heads: int = num_heads
         self.add_bias: bool = add_bias
@@ -35,11 +41,11 @@ class MultiHeadLatentAttention(torch.nn.module):
             raise ValueError(
                 "Total dimensions for Q, K, V must be divisible by num_heads."
             )
-        
+        self.head_dim = self.dim_q // num_heads
 
         self.q_down_proj_layer = Projection(
             in_features=self.d_model,
-            out_features=self.d_kv_compression,
+            out_features=self.d_q_compression,
             add_bias=False,
         )
 
@@ -50,39 +56,117 @@ class MultiHeadLatentAttention(torch.nn.module):
         )
 
         self.q_up_proj_layer = Projection(
-            in_features=self.d_kv_compression,
-            out_features=dim_q,
+            in_features=self.d_q_compression,
+            out_features=self.dim_q,
             add_bias=False,
         )
 
         self.k_up_proj_layer = Projection(
             in_features=self.d_kv_compression,
-            out_features=dim_k,
+            out_features=self.dim_k,
             add_bias=False,
         )
 
         self.v_up_proj_layer = Projection(
             in_features=self.d_kv_compression,
-            out_features=dim_v,
+            out_features=self.dim_v,
             add_bias=False,
         )
 
-        
+        self.q_rope_proj_layer = Projection(
+            in_features=self.d_q_compression,
+            out_features=self.dim_q,
+            add_bias=False,
+        )
+        self.k_rope_proj_layer = Projection(
+            in_features=self.d_model,
+            out_features=self.head_dim,
+            add_bias=False,
+        )
+
+        self.out_proj_layer = Projection(
+            in_features=self.dim_v,
+            out_features=self.d_model,
+            add_bias=add_bias,
+        )
+
+    def decoupled_rope(self, q_latent, inputs_k):
+        q_rope_proj = self.q_rope_proj_layer(q_latent)
+        k_rope_proj = self.k_rope_proj_layer(inputs_k)
+        # q_rope_proj: (batch_size, seq_len, dim_q)
+        # k_rope_proj: (batch_size, seq_len, head_dim)
+        q_rope = RotaryPositionalEncoding(d_model=self.d_model)(q_rope_proj)
+        # print("q_rope", q_rope_proj.shape, q_rope.shape)
+        # print(k_rope_proj.shape, self.head_dim)
+        k_rope = RotaryPositionalEncoding(d_model=self.head_dim)(k_rope_proj)
+        # print("k_rope", k_rope_proj.shape, k_rope.shape)
+        # q_rope shape: (batch_size, seq_len, dim_q)
+        # k_rope shape: (batch_size, seq_len, head_dim)
+        return q_rope, k_rope
+
     def forward(
         self, inputs_q: Tensor, inputs_k: Tensor, inputs_v: Tensor
     ) -> Tensor:
+        # Low-rank key-value joint compression
         # inputs shape (batch_size, seq_len, model_dims)
         q_latent = self.q_down_proj_layer(inputs_q)
         kv_latent = self.kv_down_proj_layer(inputs_k)
         # Latents shape
-        # q: (batch_size, seq_len, d_kv_compression)
+        # q: (batch_size, seq_len, d_q_compression)
         # kv: (batch_size, seq_len, d_kv_compression)
 
-        projected_q = self.q_up_proj_layer(q_latent)
-        projected_k = self.k_up_proj_layer(kv_latent)
-        projected_v = self.v_up_proj_layer(kv_latent)
+        q_proj = self.q_up_proj_layer(q_latent)
+        k_proj = self.k_up_proj_layer(kv_latent)
+        v_proj = self.v_up_proj_layer(kv_latent)
 
         # Projections Shape
         # q: (batch_size, seq_len, dim_q)
         # k: (batch_size, seq_len, dim_k)
         # v: (batch_size, seq_len, dim_v)
+
+        q_rope, k_rope = self.decoupled_rope(q_latent, inputs_k)
+
+        # Apply Rotary Positional Encoding
+        batch, seq_len, _ = q_proj.shape
+        q_proj = q_proj.view(batch, seq_len, self.num_heads, self.head_dim)
+        q_rope = q_rope.view(batch, seq_len, self.num_heads, self.head_dim)
+        # print(projected_q.shape, q_rope.shape)
+        q_new = torch.concat((q_proj, q_rope), dim=-1)
+        print(q_new.shape)
+        k_proj = k_proj.view(batch, seq_len, self.num_heads, self.head_dim)
+        k_rope = k_rope.view(batch, seq_len, 1, self.head_dim)
+        k_rope_shared = k_rope.expand(-1, -1, self.num_heads, -1)
+        # print(projected_k.shape, k_rope.shape, k_rope_shared.shape)
+        k_new = torch.concat((k_proj, k_rope_shared), dim=-1)
+        print(k_new.shape)
+
+        v_proj = v_proj.view(batch, seq_len, self.num_heads, self.head_dim)
+
+        q_new = q_new.transpose(1, 2).contiguous()
+        k_new = k_new.transpose(1, 2).contiguous()
+        v_proj = v_proj.transpose(1, 2).contiguous()
+        print(q_new.shape, k_new.shape, v_proj.shape)
+        # Shape: (batch_size, num_heads, seq_len, head_dim)
+
+        q_new = q_new.view(batch * self.num_heads, seq_len, -1)
+        k_new = k_new.view(batch * self.num_heads, seq_len, -1)
+        v_proj = v_proj.view(batch * self.num_heads, seq_len, self.head_dim)
+
+        outputs = ScaledDotProductAttention().forward(
+            q_proj=q_new,
+            k_proj=k_new,
+            v_proj=v_proj,
+            causal_mask=self.causal_mask,
+        )  # Shape: (batch * num_heads, seq_len, head_dim)
+
+        print(outputs.shape)
+
+        outputs = outputs.view(batch, self.num_heads, seq_len, self.head_dim)
+        outputs = outputs.transpose(1, 2).contiguous()
+        outputs = outputs.view(
+            batch, seq_len, self.d_model  # OR head_dim * self.num_heads
+        )
+
+        # Apply the final linear projection
+        out = self.out_proj_layer.forward(outputs)
+        return out
