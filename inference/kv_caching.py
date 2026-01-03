2789bbff3132d7bbd383099850d076e6d92b9a28
अंkur गोswami <kinglusifer1401@gmail.com>
2026-01-03 23:28:23 +0530

    support dynamic tensor caching

--- inference/kv_caching.py
+++ inference/kv_caching.py
@@ -2,20 +2,22 @@ import torch
 
 
 class KeyValueCaching(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, caching_tensor_names: list[str] | None = None):
         super().__init__()
-        self.k_proj_cache = None
-        self.v_proj_cache = None
+        for tensor_name in caching_tensor_names:
+            setattr(self, tensor_name, None)
 
-    def prefill(self, k_proj, v_proj):
-        self.k_proj_cache = k_proj
-        self.v_proj_cache = v_proj
+    def update(self, **kwargs):
+        for tensor_name, tensor in kwargs.items():
+            cached_tensor = getattr(self, tensor_name, None)
+            if cached_tensor is None:
+                # prefill stage
+                setattr(self, tensor_name, tensor)
+            else:
+                # decoding stage
+                new_cached_tensor = torch.cat([cached_tensor, tensor], dim=1)
+                setattr(self, tensor_name, new_cached_tensor)
 
-    def update(self, k_proj, v_proj):
-        if self.k_proj_cache is None or self.v_proj_cache is None:
-            self.prefill(k_proj, v_proj)
-        else:
-            self.k_proj_cache = torch.cat([self.k_proj_cache, k_proj], dim=1)
-            self.v_proj_cache = torch.cat([self.v_proj_cache, v_proj], dim=1)
-
-        return self.k_proj_cache, self.v_proj_cache
+        return tuple(
+            getattr(self, tensor_name)
+            for tensor_name in kwargs.keys())
